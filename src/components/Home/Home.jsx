43d8e6ab46ab9a43f1bea28a72b0d0a6203a7308
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-07-06 06:06:06 -0400

    added a background picture to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import background from '../../Assets/images/cryptocurrency-background.jpg';
 
 
 const Home = () => {
+
+  const styles = {
+    header: {
+      backgroundImage: `url(${background})`,
+      height: '100vh',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    },
+  
+    content: {
+      height: '100%',
+      width: '100%',
+      backgroundColor: 'rgba(50, 50, 50, .75)',
+      color: "#f0f0f0"
+      
+    }
+  }
+
+
   return (
-    <h2>Home</h2>
+    <div style={styles.header}>
+      <div style={styles.content}>
+      <h2>Home</h2>
+      </div>
+    </div>
+    
   )
 }
 
