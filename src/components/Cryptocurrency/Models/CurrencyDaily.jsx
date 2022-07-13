4dd785a2ee29c7a920b741a8af57ba0a8dd07c6e
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-07-13 06:11:39 -0400

    removed table tags  in Daily and use p tags

--- src/components/Cryptocurrency/Models/CurrencyDaily.jsx
+++ src/components/Cryptocurrency/Models/CurrencyDaily.jsx
@@ -17,37 +17,18 @@ const CurrencyDaily = (props) => {
         }=props.currencyDaily
 
   return (
-        <table border={1}>
-            <tr>
-              <th></th>
-              <th>Symbol</th>
-              <th>Price Date</th>
-              <th>Price TimeStamp</th>
-              <th>Circulating Supply</th>
-              <th>Max Supply</th>
-              <th>Market Cap</th>
-              <th>High</th>
-              <th>High TimeStamp</th>
-              <th></th>
-            </tr>
-              <tr key={id}>
-                <td>{symbol}</td>
-                <td>{price}</td>
-                <td>{priceDate}</td>
-                <td>{priceTimeStamp}</td>
-                <td>{circulatingSupply}</td>
-                <td>{maxSupply}</td>
-                <td>{marketCap}</td>
-                <td>{high}</td>
-                <td>{highTimeStamp}</td>
-                <td>
-                  <button
-                    className="waves-effect waves-light btn-small">
-                    View
-                  </button>
-                </td>
-              </tr>
-        </table>
+    <div>
+      <p>Symbol{symbol}</p>
+      <p>Price {price}</p>
+      <p>Price Date{priceDate}</p>
+      <p>Price TimeStamp {priceTimeStamp}</p>
+      <p>Circulating Supply {circulatingSupply}</p>
+      <p>Max Supply {maxSupply}</p>
+      <p>Market Cap {marketCap}</p>
+      <p>High {high}</p>
+      <p>High TimeStamp {highTimeStamp}</p>    
+    </div>
+       
   )
 }
 
