0628044a721e0ccb270a2306bf31b1519301d403
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-08-20 09:28:46 -0400

    like/unlike buttons change colors- click

--- src/components/Profiles/CurrencyHeader.jsx
+++ src/components/Profiles/CurrencyHeader.jsx
@@ -8,7 +8,9 @@ import { AuthContext } from "../Providers/AuthProvider";
 
 
 const CurrencyHeader=(props)=>{
-    const [isActive, setIsActive] = useState(false);
+    const [isLiked, setIsLiked] = useState(false);
+    const [isUnLiked, setIsUnLiked] = useState(false);
+
     const {id, name,ranking,logoUrl,symbol}=props.crypto;
     const {id: trackerId}=props.user;
     const [currencies]= useContext(CurrenciesContext);
@@ -16,9 +18,33 @@ const CurrencyHeader=(props)=>{
     const {auth}=useContext(AuthContext);
     console.log("cID: "+id);
     console.log("trackID: "+trackerId);
-    const liked=()=>{
-        alert('You Liked me!!')
-        setIsActive(current => !current);
+    const like=()=>{
+        
+        setIsLiked(current => !current);
+        if (isLiked){
+            alert(`You Liked me!!- ${isLiked}`)
+            // axios to post a like
+        }
+
+        if(!isLiked){
+            alert(`Cleared- ${isLiked}`)
+            // axios to remove/delete like
+        }
+    }
+
+    const unlike=()=>{
+        
+        setIsUnLiked(current => !current);
+        if (isUnLiked && !unlike){
+            setIsUnLiked(false);
+            alert(`You UnLiked me, why?!!- ${isUnLiked}`)
+            // axios to post a unlike
+        }
+
+        if(!isUnLiked){
+            alert(`Cleared- ${isUnLiked}`)
+            // axios to remove/delete unlike
+        }
     }
 
  
@@ -29,42 +55,24 @@ const CurrencyHeader=(props)=>{
                 <h1>Symbol: {symbol}</h1>
                 <h1>Name: {name}</h1>         
                 <div
-        style={{
-          backgroundColor: isActive ? 'salmon' : '',
-          color: isActive ? 'white' : '',
-        }}
-        onClick={liked}
-      >
-       <FontAwesomeIcon icon="fa-solid fa-thumbs-up"/> 
-      </div>
-            
+                    style={{
+                    color: isLiked ? 'black' : 'green' ,
+                    }}
+                    onClick={like}
+                >
+                 <FontAwesomeIcon icon="fa-solid fa-thumbs-up"/> 
+                </div>
+                <div
+                    style={{
+                    color: isUnLiked ? 'black' : 'red' ,
+                    }}
+                    onClick={unlike}
+                > 
+                 <FontAwesomeIcon icon="fa-solid fa-thumbs-down"/> 
+                </div>
             </div>
         </Container>  
           )
 }
 
 export default CurrencyHeader;
-
-
-{/* <Button 
-style={isLike ?{
-    width: 'auto',
-    color: '#00ff00',
-    } : {
-        width: 'auto',
-        color: '#FF00FF',
-    }
-    }
-    onClick={liked}
->
-<FontAwesomeIcon icon="fa-solid fa-thumbs-up"/>        
-</Button>
-<Button 
-style={{
-    width: 'auto',
-    color: '#ff0000',
-    }}
-    onClick={dislike}
->
-<FontAwesomeIcon icon="fa-solid fa-thumbs-down" />    
-</Button> */}
