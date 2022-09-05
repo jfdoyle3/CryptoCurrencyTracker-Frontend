f3dcfdfd8d360879e2b95aa0cd08f81b3012cb3f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-09-05 19:51:53 -0400

    test page has a select working - demo

--- src/components/Test/TestPage.jsx
+++ src/components/Test/TestPage.jsx
@@ -5,30 +5,35 @@ import '../../styling/Test.css';
 
 const TestPage =()=>{
 
-  const Countries = [
-    { label: "Albania", value: 355 },
-    { label: "Argentina", value: 54 },
-    { label: "Austria", value: 43 },
-    { label: "Cocos Islands", value: 61 },
-    { label: "Kuwait", value: 965 },
-    { label: "Sweden", value: 46 },
-    { label: "Venezuela", value: 58 }
+  const options = [
+    {
+      label: "Apple",
+      value: "apple",
+    },
+    {
+      label: "Mango",
+      value: "mango",
+    },
+    {
+      label: "Banana",
+      value: "banana",
+    },
+    {
+      label: "Pineapple",
+      value: "pineapple",
+    },
   ];
 
   return (
-    <table style={{width:'75%'}}>
-        <tr>
-          <th>Name:</th>
-          <td>Sivaraman</td>
-        </tr>
-        <tr>
-          <th>Mobile:</th>
-          <td>123456789</td>
-        </tr>
-        <tr>
-        <Select options={Countries} />
-        </tr>
-    </table>
+    <div id="App">
+        <div className="select-container">
+          <select>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+        </div>
+      </div>
   )
 }
 
